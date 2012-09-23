f9963175866e216884d4704a00da02c0bfc0b052
Eliana Feasley <e.feasley@gmail.com>
2012-09-22 23:36:24 -0500

    Initial commit for this tuorial, the first of many, many tuorials.  Accompanied by a sense of excitement and dread

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '4d9e35e877d743168a10cb58fe227c608f7cb1f02f575950edc7ba69754c8f22b36ff7a6452e61be13b1aaf4656147a24b4603b61423d04556a14aca09aa83ed'
